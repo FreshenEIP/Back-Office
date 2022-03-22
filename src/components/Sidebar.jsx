c5ca78772a7cf45f825776f71be832d9ce935ed6
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-22 18:53:12 +0100

    [ADD] sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Sidebar = () => {
+import logo from "../assets/images/logo.png"
+import sidebar_items from "../assets/JsonData/sidebar_routes.json"
+import SidebardItem from './SidebardItem'
+
+const Sidebar = props => {
+
+  const activeItem = sidebar_items.find
+
   return (
-    <div>Sidebar</div>
+    <div className={"sidebar"}>
+      <div className={"sidebar__logo"}>
+        <img src={logo} alt={"company logo"} />
+      </div>
+      {
+        sidebar_items.map((item, index) => (
+          <Link to={item.route} key={index}>
+            <SidebardItem
+              title={item.display_name}
+              icon={item.icon}
+              active={key === activeItem}
+            />
+          </Link>
+        ))
+      }
+    </div>
   )
 }
 
