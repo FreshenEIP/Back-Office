a6e1987680e3390561330d4cf1799e752cf2621c
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-23 01:51:10 +0100

    [ADD] dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import statusCard from "../assets/JsonData/status_card.json"
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <h2 className="page-header">Dashboard</h2>
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            {
+              statusCard.map((item, index) => (
+                <div className="col-6">
+                  {item.title}
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
