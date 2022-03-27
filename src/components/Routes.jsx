9828de002a227afd7cf8ec4a52ee2f04c02990c7
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-28 01:32:07 +0200

    [ADD] reports routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import { Route, Switch } from 'react-router-dom'
 
-import { Dashboard, Customers } from '../pages'
+import { Dashboard, Customers, Reports } from '../pages'
 
 const Routes = () => {
   return (
     <Switch>
       <Route path={"/"} exact component={Dashboard} />
       <Route path={"/customers"} exact component={Customers} />
+      <Route path={"/reports"} exact component={Reports} />
     </Switch>
   )
 }
