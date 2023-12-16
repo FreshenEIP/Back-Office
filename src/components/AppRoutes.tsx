3dbc873f4ad9c6ae733f7b9e395f8cadbf231ec2
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2023-12-16 16:29:02 +0100

    [UPDATE] 404.html

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-
 import {
   Brands,
   Comments,
@@ -14,7 +14,6 @@ import {
   Suggestions,
 } from '../pages';
 import { ProtectedRoutes, ProtectedRoutesAdmin } from './ProtectedRoutes';
-import React from 'react';
 
 const AppRoutes = () => {
   return (
